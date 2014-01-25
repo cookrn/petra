bc21a8d375a9889e5e4de29bd41291b198b5c989
Ryan Cook <cookrn@gmail.com>
2014-01-24 19:41:23 -0700

    add initial passing tests

--- test/petra_test.rb
+++ test/petra_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class PetraTest < Minitest::Test
-  def test_fails
-    fail
+  def test_knows_its_gem_root
+    assert_equal \
+      gem_root,
+      Petra::ROOTPATH
+  end
+
+  def test_knows_its_lib_root
+    assert_equal \
+      lib_root,
+      Petra::LIBPATH
+  end
+
+  private
+
+  def gem_root
+    @_gem_root ||=
+      File.expand_path \
+        File.join(
+          __FILE__,
+          '../../'
+        )
+  end
+
+  def lib_root
+    @_lib_root ||=
+      File.expand_path \
+        File.join(
+          gem_root,
+          'lib'
+        )
   end
 end
